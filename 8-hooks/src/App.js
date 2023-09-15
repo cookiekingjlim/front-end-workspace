9ea58d8bb460f93e009640f1d3b217bf4eb55f3f
cookiekingjlim <133620407+cookiekingjlim@users.noreply.github.com>
2023-09-15 14:03:41 +0900

    feat: React_useState/useEffect

--- 8-hooks/src/App.js
+++ 8-hooks/src/App.js
@@ -1,43 +1,9 @@
-import { useState } from "react";
-
-// const App = () => {
-//   const [counter, setCounter] = useState(0); 
-//   const plusCount = () => {
-//     setCounter(counter + 1);
-//   };
-//   const minusCount = () => {
-//     setCounter(counter - 1);
-//   };
-  
-//   return (
-//   <div>
-//     <h1>Total clicks : {counter}</h1>
-//     <button onClick = {plusCount}>+1</button>
-//     <button onClick = {minusCount}>-1</button>
-//   </div>
-//   );
-// };
-
-const Button = ({text, click}) => {
-  return  <button onClick = {click}>{text}</button>
-}
+// import Counter from "./components/Counter";
+import Converter from "./components/Converter";
 
 const App = () => {
-  const [counter, setCounter] = useState(0); 
-  const plusCount = () => {
-    setCounter(counter + 1);
-  };
-  const minusCount = () => {
-    setCounter(counter - 1);
-  };
-  
-  return (
-  <div>
-    <h1>Total clicks : {counter}</h1>
-    <Button text ="+1" click={plusCount} />
-    <Button text="-1" click={minusCount} />
-  </div>
-  );
+  // return <Counter />;
+  return <Converter />;
 };
 
 export default App;
